fab54769b1b29f343bdab6cfe1bdf625d68ac37d
Andy Morris <ajmorris@gmail.com>
2014-07-01 09:48:11 -0500

    adsfasedfasd
    asdfasdf

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = "Server=cphs-sqldev-01;Database=Compass_CORE_DEV;User ID=usr_quartz;Password=Password1*;";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+            }
+        }
+    }
+}
